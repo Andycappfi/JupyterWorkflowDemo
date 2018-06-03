cd08d871d0f13c7f663d30f5f145c580958de86d
Antero <antero@kangas.info>
2018-06-03 21:45:47 +0300

    fixed time bug

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -27,9 +27,8 @@ def get_fremont_data(filename="Fremont.csv",
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
     data = pd.read_csv(filename, index_col='Date')
-    data = pd.read_csv("Fremont.csv", index_col='Date')
     try:
-        data.index = pd.to_datetime(data.index, format="%m/%d/%Y %H:%M:%S %p" )
+        data.index = pd.to_datetime(data.index, format="%m/%d/%Y %I:%M:%S %p" )
     except TypeError:
         data.index = pd.todatetime(data.index) 
     data.columns = ["West", "East"]
